888e4759ae4396e6779c9b4456b22d7efbeeb144
Arthur Ringler <arthur.ringler@epitech.eu>
2019-11-12 18:03:04 +0100

    add logout function in dashboard & profile

--- routes/users.js
+++ routes/users.js
@@ -96,4 +96,11 @@ router.post('/register', (req, res) => {
     }
 });
 
+// Logout
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+});
+
 module.exports = router;
